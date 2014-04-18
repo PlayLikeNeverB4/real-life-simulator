c0836e6375b3728d3bfd90c86dbe6360feb20626
Adina <adinamateescu@yahoo.com>
2014-04-18 23:46:48 +0300

    Merged messages in MainWindow.

--- src/graphics/MainWindow.java
+++ src/graphics/MainWindow.java
@@ -3,7 +3,7 @@ package graphics;
 public class MainWindow {
 
     public MainWindow() {
-        System.out.println("Female print :D");
+        System.out.println("Female print :D, dar sa stii ca si ea te iubeste :*!");
         System.out.println("Esti o draguta! :*");
     }
 
