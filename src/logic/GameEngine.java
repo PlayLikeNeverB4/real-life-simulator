00ad44a108825348dee81eadbdefb2b9e3ac5777
Adina <adinamateescu@yahoo.com>
2014-04-20 22:30:47 +0300

    Initialize the fields of GameEngine class.

--- src/logic/GameEngine.java
+++ src/logic/GameEngine.java
@@ -8,11 +8,31 @@ public class GameEngine {
     private InputManager inputManager;
     private GraphicsManager graphicsManager;
 
+    public GameEngine(GraphicsManager graphicsManager) {
+        this.graphicsManager = graphicsManager;
+        inputManager = new InputManager();
+        gameWorld = new GameWorld();
+        setInitialGameWorldState();
+    }
+    
     public GameWorld getGameWorld() {
         return gameWorld;
     }
 
+    public InputManager getInputManager() {
+        return inputManager;
+    }
+
+    public GraphicsManager getGraphicsManager() {
+        return graphicsManager;
+    }
+
+    private void setInitialGameWorldState() {
+
+    }
+
     public void run() {
+
     }
 
 }
