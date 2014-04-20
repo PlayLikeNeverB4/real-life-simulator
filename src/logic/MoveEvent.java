6f57402a7f9ac5d2474de1d2bf0f272683bc0496
Adina <adinamateescu@yahoo.com>
2014-04-21 00:11:12 +0300

    Implemented MoveEvent class.

--- src/logic/MoveEvent.java
+++ src/logic/MoveEvent.java
@@ -2,14 +2,26 @@ package logic;
 
 public class MoveEvent extends AbstractEvent {
 
-    @Override
-    protected void find() {
+    private int direction;
 
+    public MoveEvent(int direction) {
+        this.direction = direction;
     }
 
     @Override
-    protected void updateWorldObject() {
+    protected AbstractObject find() {
+        return this.gameWorld.getMainCharacter();
+    }
 
+    @Override
+    protected void updateWorldObject(AbstractObject abstractObject) {
+        switch (direction) {
+            case 0: abstractObject.updatePosition(0,0,0); // up
+            case 1: abstractObject.updatePosition(0,0,0); // down
+            case 2: abstractObject.updatePosition(0,0,0); // left
+            case 3: abstractObject.updatePosition(0,0,0); // right
+            default: System.out.println("Error! Option does not exist!");
+        }
     }
 
 }
