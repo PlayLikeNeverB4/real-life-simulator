2d4d636ca9495a1fe3e8ea8b7f9fa83876107913
Adina <adinamateescu@yahoo.com>
2014-04-21 14:12:53 +0300

    Added javaDoc and directions.

--- src/logic/InputManager.java
+++ src/logic/InputManager.java
@@ -7,69 +7,84 @@ import java.awt.event.MouseMotionListener;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * This class manages all the input events fired by pressing a key or by moving the mouse
+ */
 public class InputManager implements MouseMotionListener, KeyListener {
 
+    /**
+     * A LinkedList that contains all the events which appear during execution of the application
+     */
     private List<AbstractEvent> eventList;
-    private static final int UP = 0;
-    private static final int DOWN = 1;
-    private static final int LEFT = 2;
-    private static final int RIGHT = 3;
 
     public InputManager() {
         eventList = new LinkedList<AbstractEvent>();
     }
 
+    /**
+     * This method copies and then deletes the existing list of events and then return the copy list of events
+     * @return the list of events created in one frame
+     */
     public synchronized List<AbstractEvent> popNewEvents() {
-        List<AbstractEvent> cpyEventList = eventList;
+        List<AbstractEvent> cpyEventList = new LinkedList<AbstractEvent>();
+        for(int i = 0; i < eventList.size(); i++) {
+            AbstractEvent event = eventList.get(i);
+            if (event instanceof MoveEvent)
+                cpyEventList.add(new MoveEvent((MoveEvent) event));
+            else if(event instanceof FireEvent) {
+                cpyEventList.add(new FireEvent((FireEvent) event));
+            } else if(event instanceof SwitchEvent) {
+                cpyEventList.add(new SwitchEvent((SwitchEvent) event));
+            }
+        }
         eventList.clear();
         return cpyEventList;
-    }
-
-    @Override
-    public void keyTyped(KeyEvent e) {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
         int key = e.getKeyCode();
         switch (key) {
-            case KeyEvent.VK_UP : eventList.add(new MoveEvent(UP));
+            case KeyEvent.VK_UP : eventList.add(new MoveEvent(MoveEvent.FORWARD));
                 break;
-            case KeyEvent.VK_DOWN : eventList.add(new MoveEvent(DOWN));
+            case KeyEvent.VK_DOWN : eventList.add(new MoveEvent(MoveEvent.BACK));
                 break;
-            case KeyEvent.VK_LEFT : eventList.add(new MoveEvent(LEFT));
+            case KeyEvent.VK_LEFT : eventList.add(new MoveEvent(MoveEvent.LEFT));
                 break;
-            case KeyEvent.VK_RIGHT : eventList.add(new MoveEvent(RIGHT));
+            case KeyEvent.VK_RIGHT : eventList.add(new MoveEvent(MoveEvent.RIGHT));
                 break;
-            case KeyEvent.VK_V : eventList.add(new MoveEvent(UP));
+            case KeyEvent.VK_W : eventList.add(new MoveEvent(MoveEvent.FORWARD));
                 break;
-            case KeyEvent.VK_S : eventList.add(new MoveEvent(DOWN));
+            case KeyEvent.VK_S : eventList.add(new MoveEvent(MoveEvent.BACK));
                 break;
-            case KeyEvent.VK_A : eventList.add(new MoveEvent(LEFT));
+            case KeyEvent.VK_A : eventList.add(new MoveEvent(MoveEvent.LEFT));
                 break;
-            case KeyEvent.VK_D : eventList.add(new MoveEvent(RIGHT));
+            case KeyEvent.VK_D : eventList.add(new MoveEvent(MoveEvent.RIGHT));
                 break;
             case KeyEvent.VK_F : eventList.add(new FireEvent());
                 break;
             case KeyEvent.VK_E : eventList.add(new SwitchEvent());
                 break;
-            default: System.out.println("This key is not a valid one!");
+            default: {
+                System.out.println("This key is not a valid one!");
+            }
         }
     }
 
     @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
     public void keyReleased(KeyEvent e) {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 
     @Override
     public void mouseDragged(MouseEvent e) {
-        //To change body of implemented methods use File | Settings | File Templates.
     }
 
-    @Override
-    public void mouseMoved(MouseEvent e) {
-        //To change body of implemented methods use File | Settings | File Templates.
-    }
 }
