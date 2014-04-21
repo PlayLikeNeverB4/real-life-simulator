d98c6e34d9ed0aee042cf6026e06375e904e32fd
Adina <adinamateescu@yahoo.com>
2014-04-21 14:08:33 +0300

    Renaming the find function and added java doc.

--- src/logic/AbstractEvent.java
+++ src/logic/AbstractEvent.java
@@ -1,16 +1,34 @@
 package logic;
 
+/**
+ * This class refers to all events that can appear into the application
+ */
 public abstract class AbstractEvent {
+    /**
+     * Finds the object from GameWorld that will be updated.
+     * @return an instance of a subclass of AbstractObject class
+     */
+    protected abstract AbstractObject findObject();
 
-    protected abstract AbstractObject find();
-    protected abstract void updateWorldObject(AbstractObject abstractObject);
+    /**
+     * Updates the object given as argument from GameWorld
+     * @param targetObject this object will be updated
+     * @throws Exception
+     */
+    protected abstract void updateWorldObject(AbstractObject targetObject) throws Exception;
 
+    /**
+     * The GameWorld for which it updates the object
+     */
     protected GameWorld gameWorld;
 
-    public void handle() {
-        AbstractObject abstractObject = find();
-        if(abstractObject != null) {
-            updateWorldObject(abstractObject);
+    /**
+     * This method handle the event by finding the object and updating it as the event ask
+     */
+    public void handle() throws Exception {
+        AbstractObject targetObject = findObject();
+        if(targetObject != null) {
+            updateWorldObject(targetObject);
         }
     }
 
