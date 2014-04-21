b94d8cf6f14a1516112d582addceac950afa1342
Adina <adinamateescu@yahoo.com>
2014-04-21 14:13:13 +0300

    Renaming the inherited methods

--- src/logic/MoveEvent.java
+++ src/logic/MoveEvent.java
@@ -1,26 +1,44 @@
 package logic;
 
 public class MoveEvent extends AbstractEvent {
+    /**
+     * FORWARD, BACK, LEFT and RIGHT are the four possible directions into which that the MainCharacter can goes
+     */
+    public static final int FORWARD = 0;
+    public static final int BACK = 1;
+    public static final int LEFT = 2;
+    public static final int RIGHT = 3;
 
+    /**
+     * This field shows the direction to which the object will be moved
+     */
     private int direction;
 
+    /**
+     * Copy constructor
+     * @param direction
+     */
     public MoveEvent(int direction) {
         this.direction = direction;
     }
 
+    public MoveEvent(MoveEvent moveEvent) {
+        this.direction = moveEvent.direction;
+    }
+
     @Override
-    protected AbstractObject find() {
+    protected AbstractObject findObject() {
         return this.gameWorld.getMainCharacter();
     }
 
     @Override
-    protected void updateWorldObject(AbstractObject abstractObject) {
+    protected void updateWorldObject(AbstractObject abstractObject) throws Exception {
         switch (direction) {
-            case 0: abstractObject.updatePosition(0,0,0); // up
-            case 1: abstractObject.updatePosition(0,0,0); // down
-            case 2: abstractObject.updatePosition(0,0,0); // left
-            case 3: abstractObject.updatePosition(0,0,0); // right
-            default: System.out.println("Error! Option does not exist!");
+            case FORWARD: abstractObject.updatePosition(0,0,0);
+            case BACK: abstractObject.updatePosition(0,0,0);
+            case LEFT: abstractObject.updatePosition(0,0,0);
+            case RIGHT: abstractObject.updatePosition(0,0,0);
+            default: throw new Exception("Error! Option does not exist!");
         }
     }
 
