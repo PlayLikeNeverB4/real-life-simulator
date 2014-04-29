25956d2d08032e838ec901756fc4c5212986a524
George <george_marcus92@yahoo.com>
2014-04-29 23:31:24 +0300

    Added Dimension class.

---
+++ src/logic/Dimension.java
@@ -0,0 +1,40 @@
+package logic;
+
+/**
+ * Represents a 3D dimension in the game world
+ */
+public class Dimension {
+    private double x;
+    private double y;
+    private double z;
+
+    public Dimension(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getZ() {
+        return z;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public void setZ(double z) {
+        this.z = z;
+    }
+}
